478a4b82327288832d2a62a4a9093dff6e8855ae
Amanullah23 <amanyawari22@gmail.com>
2025-11-22 14:36:13 +0430

    mobulel nav

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -17,7 +17,7 @@ const MobileNav = ({ closeNav, showNav }: Props) => {
         className={`fixed ${navOpen} inset-0 transform transition-all right-0 duration-500 z-100002 bg-black opacity-70 w-full h-screen`}
       ></div>
       <div
-        className={`text-white ${navOpen} fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 2-[80%] sm:w-[60%] bg-cyan-800 space-7-6 z-1000050 right-0`}
+        className={`text-white ${navOpen} fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-cyan-800 space-y-6 z-[1000050] right-0`}
       >
         {Navlinks.map((link) => {
           return (
